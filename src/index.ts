fad7182d8cf64899e5e39267d79a7fb9596a25f1
soleu <dlthf555@gmail.com>
2021-06-16 01:56:30 +0900

    [fix] cors 오류 수정

--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,13 @@ app.use(cors());
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err.message; 
   res.locals.error = req.app.get("env") === "production" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
-
-
 
 app.listen(5000, () => {
     console.log(`
